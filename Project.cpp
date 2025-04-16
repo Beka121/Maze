387cdb653eb02ca5b3ec5aa81028466d6839c1db
Claimer <95367083+Beka121@users.noreply.github.com>
2025-04-16 22:46:24 +0600

    Random function

--- Project.cpp
+++ Project.cpp
@@ -1,25 +1,28 @@
-//Fisher YAtes
-        while (!st.empty()) {
-            Point current = st.top();
-            vector<int> directions = {0, 1, 2, 3};
-            random_shuffle(directions.begin(), directions.end());
+//random exit and start 
+    void placeStartExit() {
+        vector<Point> borders;
+        for (int i = 1; i < SIZE-1; i++) {
+            borders.emplace_back(i, 1);
+            borders.emplace_back(i, SIZE-2);
+            borders.emplace_back(1, i);
+            borders.emplace_back(SIZE-2, i);
+        }
+        random_shuffle(borders.begin(), borders.end());
 
-            bool moved = false;
-            for (int dir : directions) {
-                int nx = current.x + dx[dir] * 2;
-                int ny = current.y + dy[dir] * 2;
+        start = borders[0];
+        exit = borders[1];
+        grid[start.y][start.x] = START;
+        grid[exit.y][exit.x] = EXIT;
+    }
 
-                if (nx > 0 && nx < SIZE-1 && ny > 0 && ny < SIZE-1 && grid[ny][nx] == WALL) {
-                    grid[ny][nx] = PATH;
-                    grid[current.y + dy[dir]][current.x + dx[dir]] = PATH;
-                    st.push(Point(nx, ny));
-                    moved = true;
-                    break;
-                }
+    void print() const {
+        for (int y = 0; y < SIZE; y++) {
+            for (int x = 0; x < SIZE; x++) {
+                if (grid[y][x] == START) cout << START << ' ';
+                else if (grid[y][x] == EXIT) cout << EXIT << ' ';
+                else if (solutionPath[y][x]) cout << SOLUTION << ' ';
+                else cout << grid[y][x] << ' ';
             }
-
-            if (!moved) {
-                st.pop();
-            }
+            cout << endl;
         }
     }
