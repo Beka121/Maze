f22aa80dba43c4969195321a47f8dcdada3a8456
Claimer <95367083+Beka121@users.noreply.github.com>
2025-04-16 22:44:50 +0600

    DFS with stacks

--- Project.cpp
+++ Project.cpp
@@ -1,11 +1,8 @@
-void print() const {
-    for (int y = 0; y < SIZE; y++) {
-        for (int x = 0; x < SIZE; x++) {
-            if (grid[y][x] == START) cout << START << ' ';
-            else if (grid[y][x] == EXIT) cout << EXIT << ' ';
-            else if (solutionPath[y][x]) cout << SOLUTION << ' ';
-            else cout << grid[y][x] << ' ';
-        }
-        cout << endl;
-    }
-}
+//dfs
+    void generate() {
+        stack<Point> st;
+        st.push(Point(1, 1));
+        grid[1][1] = PATH;
+//dfs stack 
+        int dx[] = {0, 1, 0, -1};
+        int dy[] = {-1, 0, 1, 0};
