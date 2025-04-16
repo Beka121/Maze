2b3dfc8832da8d02e0259bdd1d7f8ace06f755b9
Claimer <95367083+Beka121@users.noreply.github.com>
2025-04-16 22:45:20 +0600

    Fisher YAtes Algorithm

--- Project.cpp
+++ Project.cpp
@@ -1,8 +1,25 @@
-//dfs
-    void generate() {
-        stack<Point> st;
-        st.push(Point(1, 1));
-        grid[1][1] = PATH;
-//dfs stack 
-        int dx[] = {0, 1, 0, -1};
-        int dy[] = {-1, 0, 1, 0};
+//Fisher YAtes
+        while (!st.empty()) {
+            Point current = st.top();
+            vector<int> directions = {0, 1, 2, 3};
+            random_shuffle(directions.begin(), directions.end());
+
+            bool moved = false;
+            for (int dir : directions) {
+                int nx = current.x + dx[dir] * 2;
+                int ny = current.y + dy[dir] * 2;
+
+                if (nx > 0 && nx < SIZE-1 && ny > 0 && ny < SIZE-1 && grid[ny][nx] == WALL) {
+                    grid[ny][nx] = PATH;
+                    grid[current.y + dy[dir]][current.x + dx[dir]] = PATH;
+                    st.push(Point(nx, ny));
+                    moved = true;
+                    break;
+                }
+            }
+
+            if (!moved) {
+                st.pop();
+            }
+        }
+    }
