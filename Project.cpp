7f71d193dbed4809219cd9d55c7c51bf47528588
Claimer <95367083+Beka121@users.noreply.github.com>
2025-04-16 22:47:10 +0600

    Check for errors

--- Project.cpp
+++ Project.cpp
@@ -1,4 +1,24 @@
-bool solve() {
-        vector<vector<bool>> visited(SIZE, vector<bool>(SIZE, false));
-        return findPath(start.x, start.y, visited);
+private:
+    bool findPath(int x, int y, vector<vector<bool>>& visited) {
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE || visited[y][x] || grid[y][x] == WALL) {
+            return false;
+        }
+
+        visited[y][x] = true;
+
+        if (Point(x, y) == exit) {
+            solutionPath[y][x] = true;
+            return true;
+        }
+
+        if (findPath(x+1, y, visited) || findPath(x-1, y, visited) || 
+            findPath(x, y+1, visited) || findPath(x, y-1, visited)) {
+            if (grid[y][x] == PATH) {
+                solutionPath[y][x] = true;
+            }
+            return true;
+        }
+
+        return false;
     }
+};
