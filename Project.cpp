7e8956f870cf9395f2bc7b2eb04621c62147f3b2
Claimer <95367083+Beka121@users.noreply.github.com>
2025-04-16 22:46:50 +0600

    Recursive

--- Project.cpp
+++ Project.cpp
@@ -1,28 +1,4 @@
-//random exit and start 
-    void placeStartExit() {
-        vector<Point> borders;
-        for (int i = 1; i < SIZE-1; i++) {
-            borders.emplace_back(i, 1);
-            borders.emplace_back(i, SIZE-2);
-            borders.emplace_back(1, i);
-            borders.emplace_back(SIZE-2, i);
-        }
-        random_shuffle(borders.begin(), borders.end());
-
-        start = borders[0];
-        exit = borders[1];
-        grid[start.y][start.x] = START;
-        grid[exit.y][exit.x] = EXIT;
-    }
-
-    void print() const {
-        for (int y = 0; y < SIZE; y++) {
-            for (int x = 0; x < SIZE; x++) {
-                if (grid[y][x] == START) cout << START << ' ';
-                else if (grid[y][x] == EXIT) cout << EXIT << ' ';
-                else if (solutionPath[y][x]) cout << SOLUTION << ' ';
-                else cout << grid[y][x] << ' ';
-            }
-            cout << endl;
-        }
+bool solve() {
+        vector<vector<bool>> visited(SIZE, vector<bool>(SIZE, false));
+        return findPath(start.x, start.y, visited);
     }
